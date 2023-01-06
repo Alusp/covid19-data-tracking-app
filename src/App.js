507b50780dd5f12f8adff18155083b45e4ae8838
Alusp <alusinejalloh800@gmail.com>
2023-01-06 17:52:51 +0200

    Add footer and fixed filter error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 // import './index.css';
 import Dashboard from './components/Dashboard';
 import CountryDetails from './components/CountryDetails';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/" element={<Dashboard />} />
         <Route path="/details/:id" element={<CountryDetails />} />
       </Routes>
+      <Footer />
     </>
   );
 }
